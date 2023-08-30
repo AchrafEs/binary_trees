052533a7ef5fb4ac6e2f6f51ee8fa0b74fb6060b
AchrafEs <achraf.es2110@gmail.com>
2023-08-30 13:41:02 +0100

    updated the file

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - a function that creates a binary tree node
+ * binary_tree_node - function that creates a binary tree node
  * @parent: the parent node
  * @value: the value of the node
  *

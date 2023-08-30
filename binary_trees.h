bfa73cf01f56cccc219c61c695c29eb00b048f7f
AchrafEs <achraf.es2110@gmail.com>
2023-08-30 15:28:22 +0100

    updated the header file

--- binary_trees.h
+++ binary_trees.h
@@ -42,7 +42,7 @@ typedef struct link_s
 	struct binary_tree_s const *node;
 	struct link_s *next;
 } link_t;
-/*  ----------------- prototypes for functions ---------------------------------*/
+/*  ----------------- functions prototypes ---------------------------------*/
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
